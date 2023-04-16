f221ee7db149dfc7b3bc653364aefd44f43e00ac
Patrick Weiss <patrick.weiss@wu.ac.at>
2023-04-16 17:42:41 +0200

    April 2023 paper update

--- Code/z_options_figures.R
+++ Code/z_options_figures.R
@@ -2,9 +2,12 @@
 ## Contains the theme and layout options for figures
 
 # Ggplot options
-theme_set(theme_minimal() + theme(legend.position = "bottom"))
+theme_set(theme_minimal() + 
+            theme(legend.position = "bottom") +
+            theme(axis.title = element_text(size = 10)) +
+            theme(plot.subtitle = element_text(size = 10)))
 
-pal <- colorRampPalette(list("#DD8D29", "#DFB411", "#CBCC1C", "#72B68E", "#73A18F", "#CE8B1D", "#D0530E", "#B40F20")) # FantasticFox1
+pal <- colorRampPalette(list("#DD8D29", "#DFB411", "#CBCC1C", "#72B68E", "#52806e", "#CE8B1D", "#D0530E", "#B40F20")) # FantasticFox1
 
 scale_colour_continuous <- function(...) scale_color_gradientn(colours = pal(20), ...)
 scale_colour_discrete   <- function(...) discrete_scale("colour", scale_name = "pal", palette = pal, ...)
